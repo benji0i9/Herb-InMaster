db9797b80a5c20cbe9f9700626f2c458152259cc
rob <robsanchez1772@gmail.com>
2022-07-21 12:55:53 -0700

    updates

--- components/Water.js
+++ components/Water.js
@@ -1,170 +1,181 @@
-import * as React from 'react'
-import { StyleSheet, Text, View, FlatList, Image, Button, TouchableHighlight } from 'react-native'
-import { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Slider from '@react-native-community/slider'
-import InfoPage from './Info'
-// import { TouchableHighlight } from 'react-native-gesture-handler'
-// import { NavigationContainer } from '@react-navigation/native'
+import * as React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image,
+  Button,
+  TouchableHighlight,
+} from "react-native";
+import { useState } from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function WaterPlant({ navigation }) {
-  const shovelPic = require('../assets/shovel.png');
-  
-  const [showRectangle, setShowRectangle] = useState(true)
+  const shovelPic = require("../assets/shovel.png");
+  const whitePlus = require("../assets/shovel.png");
+
+  const [showRectangle, setShowRectangle] = useState(true);
   const [plantList, setPlantList] = useState([
-
-
-
     //1
     {
-      name: 'Sunflower',
-      img: require('../assets/sunflower.png'),
+      name: "Sunflower",
+      img: require("../assets/sunflower.png"),
     },
     //2
     {
-      name: 'Dandylion',
-      img: require('../assets/dandylion.png'),
+      name: "Dandylion",
+      img: require("../assets/dandylion.png"),
     },
     //3
     {
-    
-      name: 'Cactus',
-      img: require('../assets/Cactus.png'),
+      name: "Cactus",
+      img: require("../assets/Cactus.png"),
     },
     //4
     {
-      
-      name: 'Daisy',
-      img: require('../assets/daisy.png'),
+      name: "Daisy",
+      img: require("../assets/daisy.png"),
     },
     //5
     {
-      name: 'Rose',
-      img: require('../assets/Rose.png'),
+      name: "Rose",
+      img: require("../assets/Rose.png"),
     },
     //6
     {
-      name: 'cone flower',
-      img: require('../assets/Coneflower.png'),
+      name: "cone flower",
+      img: require("../assets/Coneflower.png"),
     },
     //7
     {
-      name: 'Catmint',
-      img: require('../assets/Catmint.png'),
+      name: "Catmint",
+      img: require("../assets/Catmint.png"),
     },
     //8
     {
-      name: 'Agastache',
-      img: require('../assets/Agastache1.png'),
+      name: "Agastache",
+      img: require("../assets/Agastache1.png"),
     },
     //9
     {
-      name: 'lantana',
-      img: require('../assets/Lantana.png'),
+      name: "lantana",
+      img: require("../assets/Lantana.png"),
     },
     //10
     {
-      name: 'Aloe Vera',
-      img: require('../assets/Aloevera.png'),
+      name: "Aloe Vera",
+      img: require("../assets/Aloevera.png"),
     },
-  ])
+  ]);
 
   const changeShape = (shape) => {
-    setShowRectangle(shape == 'Rectangle')
-  }
+    setShowRectangle(shape == "Rectangle");
+  };
 
   const FlatListItemSeparator = () => {
     return (
       <View
         style={{
           height: 30,
-          width: '100%',
+          width: "100%",
           // backgroundColor: '#000',
         }}
       />
-    )
-  }
+    );
+  };
   //  const goWaterLevel = () => {
   //    navigation.navigate('WaterLevel')
   //  }
   return (
     <View style={styles.container}>
-      <SafeAreaView>
-        <FlatList
-          data={plantList}
-          ItemSeparatorComponent = {FlatListItemSeparator}
-          renderItem={({ item }) => (
-            <>
-              <View style={showRectangle ? styles.RectangleShapeView : ''}>
+      <FlatList
+        data={plantList}
+        header
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        ItemSeparatorComponent={FlatListItemSeparator}
+        ListHeaderComponent={() => (
+          <View style={styles.plusView}>
+            <TouchableHighlight
+              style={{
+                backgroundColor: "#000000",
+                alignSelf: "center",
+                width: 45,
+                height: 45,
+                borderRadius: 5,
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+              onPress={() => navigation.navigate("Plus", item)}
+            >
+              <FontAwesome5 name="plus" size={24} color="#fff" />
+            </TouchableHighlight>
+          </View>
+        )}
+        renderItem={({ item }) => (
+          <>
+            <View style={showRectangle ? styles.RectangleShapeView : ""}>
+              <Image
+                style={{
+                  alignSelf: "center",
+                  width: 160,
+                  height: 160,
+                  top: -10,
+                }}
+                source={item.img}
+              />
+
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  textAlign: "center",
+                }}
+              >
+                {" "}
+                Name: {item.name}
+              </Text>
+
+              <TouchableHighlight
+                onPress={() => navigation.navigate("Info", item)}
+              >
                 <Image
                   style={{
-                    alignSelf: 'center',
-                    width: 160,
-                    height: 160,
-                    top: -10,
+                    backgroundColor: "#000000",
+                    alignSelf: "center",
+                    width: 55,
+                    height: 55,
+                    borderRadius: 5,
                   }}
-                  source={item.img}
+                  source={shovelPic}
                 />
-
-                <Text
-                  style={{
-                    fontWeight: 'bold',
-                    textAlign: 'center',
-                  }}
-                >
-                  {' '}
-                  Name: {item.name}
-                </Text>
-
-
-                <TouchableHighlight onPress={() => navigation.navigate('Info', item)}>
-                  <Image
-                    style={{
-                      backgroundColor: '#000000',
-                      alignSelf: 'center',
-                      width: 55,
-                      height: 55,
-                      borderRadius: 5,
-                    }}
-                    source={shovelPic}
-                  />
-               
-                </TouchableHighlight>
-              </View>
-            </>
-          )} 
-        />
-       
-      </SafeAreaView>
+              </TouchableHighlight>
+            </View>
+          </>
+        )}
+      />
     </View>
-  )
+  );
 }
 
-
 const styles = StyleSheet.create({
-  contained: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContents: 'Center',
-  },
-
-  // textContainer: {
-  //   backgroundColor: 'red',
-  //   margin: 40,
-  //   //  borderBottomWidth: 'medium'
-  // },
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 15,
   },
   RectangleShapeView: {
     //To make Rectangle Shape
-    marginTop: 20,
+    marginBottom: 20,
     width: 300,
     height: 220,
-    backgroundColor: '#D3F1D5',
+    backgroundColor: "#D3F1D5",
     borderRadius: 19,
   },
-  
-})
+  plusView: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginBottom: 20,
+  },
+});
