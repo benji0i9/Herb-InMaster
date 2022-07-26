94adcc00fd307e142057fcb598ddd4a65c098939
TXT-45 <txt-45@TXT-45.local>
2022-07-25 20:01:31 -0700

    message

--- components/Info.js
+++ components/Info.js
@@ -8,52 +8,69 @@ import {
   TouchableHighlight,
   Image,
   TouchableOpacity,
+  AppRegistry,
 } from 'react-native'
-import { useState } from 'react'
-// import { TouchableHighlight } from 'react-native-gesture-handler'
+import { useState } from 'react';
+// import Slider from '@react-native-community/slider'
+import Slider from '@react-native-community/slider'
+
 
 export default function InfoPage({ route, navigation }) {
   const arrow = require('../assets/backarrow.png')
+  const Vector= require('../assets/Vector.png')
+  const [sliderValue, setSliderValue] = useState(0)
   const [showRectangle, setShowRectangle] = useState(true)
   return (
-    <View style={showRectangle ? styles.RectangleShapeView : ''}>
-      <View style={{ alignContent: 'center' }}>
-        {/* Plant Name Display */}
+    <View style={{ alignContent: 'center' }}>
+      {/* Plant Name Display */}
 
-        {/* Back Arrow */}
-        <View style={{ flexDirection: 'row', padding: 22 }}>
-          <TouchableOpacity
-            activeOpacity={1}
-            style={{ top: 67, width: 20, height: -10, marginTop: -40 }}
-            onPress={() => navigation.goBack()}
-          >
-            <Image
-              style={{
-                alignSelf: 'center',
-                width: 35,
-                height: 35,
-                borderRadius: 5,
-                top: -1,
-              }}
-              source={arrow}
-            />
-          </TouchableOpacity>
-          <Text style={styles.Names}> {route.params.name} </Text>
-        </View>
-
+      {/* Back Arrow */}
+      <View style={{ flexDirection: 'row', padding: 22 }}>
+        <TouchableOpacity
+          activeOpacity={1}
+          style={{ top: 67, width: 20, height: -10, marginTop: -40 }}
+          onPress={() => navigation.goBack()}
+        >
+          <Image
+            style={{
+              alignSelf: 'center',
+              width: 35,
+              height: 35,
+              borderRadius: 5,
+              top: -1,
+            }}
+            source={arrow}
+          />
+        </TouchableOpacity>
+        <Text style={styles.Names}> {route.params.name} </Text>
+      </View>
+      <View style={showRectangle ? styles.RectangleShapeView : ''}>
         {/* Param Plant Image */}
         <Image
           style={{
             alignSelf: 'center',
             width: 300,
             height: 300,
-            top: 100,
-            left: 100,
+            top: 40,
+            left: -70,
           }}
           source={route.params.img}
         />
       </View>
-    </View>
+      <View>
+        <Text>Water Control</Text>
+        <Image
+          style={{
+            // left:10,
+            width: 50,
+            height: 70,
+            right: -30,
+          }}
+          source={Vector}
+        />
+  
+        </View>
+      </View>
   )
 }
 
@@ -73,10 +90,22 @@ const styles = StyleSheet.create({
   RectangleShapeView: {
     //To make Rectangle Shape
     marginBottom: 20,
-    width: 300,
-    height: 220,
+    width: 400,
+    height: 400,
     backgroundColor: '#D3F1D5',
-    borderRadius: 19,
-    
-  },
+    borderRadius: 200,
+    top:10,
+    left:110,
+    ...Platform.select({
+      ios: {
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.2,
+          shadowRadius: 1,    
+      },
+      android: {
+          elevation: 10,
+      },
+      }),
+   },
 })
